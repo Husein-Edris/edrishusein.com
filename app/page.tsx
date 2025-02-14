9f2aa623ba08aafc73acaf286411c5be298f5266
Edris <edris.husein0@gmail.com>
2025-02-14 18:40:02 +0100

    project setup + component setup

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Header from '../src/components/Header/Header';
+
+export default function Home() {
+  return (
+    <main>
+      <Header />
+      {/* Your other page content */}
+    </main>
+  );
+}
---
+++ src/lib/apollo-client.ts
@@ -0,0 +1,18 @@
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+    uri: 'https://cms.edrishusein.com/graphql',
+    cache: new InMemoryCache(),
+    defaultOptions: {
+        watchQuery: {
+            fetchPolicy: 'no-cache',
+            errorPolicy: 'ignore',
+        },
+        query: {
+            fetchPolicy: 'no-cache',
+            errorPolicy: 'all',
+        },
+    },
+});
+
+export default client;
