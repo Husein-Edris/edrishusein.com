b4fd739b2cb65e764fa427bd528f3fdc9ebf1071
Edris <edris.husein0@gmail.com>
2025-02-15 17:40:58 +0100

    cards section with some fixes

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Hero from '@/src/components/Hero/Hero';
 import Header from '../src/components/Header/Header';
 import Projects from '@/src/components/Projects/Projects';
 import About from '@/src/components/about/about';
+import InfoCards from '@/src/components/InfoCards/InfoCards';
 
 export default function Home() {
   return (
@@ -10,6 +11,24 @@ export default function Home() {
       <Hero />
       <Projects />
       <About />
+      <InfoCards
+        columns={2}
+        cards={[
+          {
+            title: "BOOKSHELF",
+            description: "Books and pieces of wisdom I've enjoyed reading",
+            image: "/images/books-bg.png",
+            link: "/bookshelf"
+          },
+          {
+            title: "TECH STACK",
+            description: "The dev tools, apps, devices, and games I use and play with",
+            image: "/images/tech-bg.png",
+            link: "/tech-stack"
+          }
+        ]}
+      />
+
     </main>
 
   );
