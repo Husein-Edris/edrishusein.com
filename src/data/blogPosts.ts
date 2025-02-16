7b32c8a20a5c41765a9cfa8f0c142a240d5b7058
Edris <edris.husein0@gmail.com>
2025-02-16 22:45:36 +0100

    Fixed Bugs on infoCards Component

---
+++ src/data/blogPosts.ts
@@ -0,0 +1,20 @@
+export const blogPosts = [
+    {
+      title: "Headless WordPress and Next.js: benefits and setup",
+      description: "The world's most popular content management system, WordPress, has evolved past ...",
+      image: "/images/Blog-sample-img.png",
+      link: "/blog/headless-wordpress"
+    },
+    {
+      title: "Headless WordPress and Next.js: benefits and setup",
+      description: "The world's most popular content management system, WordPress, has evolved past ...",
+      image: "/images/Blog-sample-img.png",
+      link: "/blog/headless-wordpress-2"
+    },
+    {
+      title: "Headless WordPress and Next.js: benefits and setup",
+      description: "The world's most popular content management system, WordPress, has evolved past ...",
+      image: "/images/Blog-sample-img.png",
+      link: "/blog/headless-wordpress-3"
+    }
+  ];
