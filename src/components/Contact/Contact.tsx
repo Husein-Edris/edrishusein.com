9e1375b5891aa74060dde6a201d19532a8eee311
Edris <edris.husein0@gmail.com>
2025-02-20 23:29:34 +0100

    Contact & Footer

---
+++ src/components/Contact/Contact.tsx
@@ -0,0 +1,20 @@
+import SectionHeader from '../SectionHeader/SectionHeader';
+import './Contact.scss';
+
+const Contact = () => {
+  return (
+    <section className="contact">
+      <div className="container">
+        <div className="contact-content">
+          <h3 className="subtitle">WANT TO WORK TOGETHER?</h3>
+          <h2 className="title">SAY HI!</h2>
+          <a href="mailto:kontakt@edrishusein.com" className="email">
+            KONTAKT@EDRISHUSEIN.COM
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
