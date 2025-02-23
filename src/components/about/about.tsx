49b259fb7e4dfde409718febdeda923c1c403e76
Edris <edris.husein0@gmail.com>
2025-02-24 00:36:19 +0100

    About section changes

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -5,9 +5,25 @@ const About = () => {
   return (
     <section className="about">
       <div className="container">
-        <SectionHeader number="02" title="About Me" variant="light"/>
+        <SectionHeader number="02" title="About Me" variant="light" />
         <p className="description">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed libero metus, vehicula sit amet orci at, consequat lobortis ex. Etiam fermentum lacinia viverra. Nam tristique iaculis tincidunt. Vestibulum pellentesque, quam et tempor blandit, nisl ligula dictum quam, ac dapibus ipsum felis eget tortor. Ut cursus rutrum lorem, vel laoreet felis convallis eu. Proin dapibus ligula a pretium rutrum. Donec eu molestie erat,
+          <p>I'm Edris, a WordPress Developer based in Dornbirn, Austria. I specialize in WordPress development, PHP, and modern web technologies, always looking for ways to build better, faster, and more efficient web solutions.</p>
+
+          <h3>What I do</h3>
+          <p> Currently, I'm working on freelance projects, helping businesses and individuals create functional and scalable websites. My focus is on custom WordPress solutions, headless CMS setups, and performance optimization.</p>
+
+          <h3>What I did</h3>
+          <p>Previously, I worked as a Junior Web Developer at <a href="https://www.bap.cc/" target="_blank">Baschnegger Ammann Partner Werbeagentur</a>, where I contributed to projects for clients such as:</p>
+
+          <ul className='project-list'>
+            <li><a href="https://www.fehr-technik.com/" target="_blank">Fehr Lagerlogistik</a></li>
+            <li><a href="https://geschaeftsbericht2022.landeskrankenhaus.at/" target="_blank">Vorarlberger Landeskrankenhäuser</a></li>
+            <li><a href="https://technikland.at/" target="_blank">Technikland Vorarlberg</a></li>
+          </ul>
+
+          <p>Before that, I worked with <a href="https://bobdo.at/" target="_blank">bobdo</a>, focusing on Elementor, CSS, and PHP customizations.</p>
+
+          <p>I'm currently expanding my expertise in React, Next.js, and headless WordPress to push the boundaries of modern web development.</p>
         </p>
       </div>
     </section>
